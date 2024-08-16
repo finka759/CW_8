3f207a7f75648ef6d12fec972a80a3fe80945c13
finka759 <finka759@gmail.com>
2024-08-16 23:57:56 +0300

    устранение замечаний+password

--- users/serializers.py
+++ users/serializers.py
@@ -7,6 +7,7 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 4}}
         # fields = [
         #     'id',
         #     'email',
